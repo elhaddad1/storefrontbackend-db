0908c28cebe635ba68fd81cef11fbf80f416af6e
elhaddad1 <a.s.elhaddad@gmail.com>
2022-12-17 15:29:22 +0300

    fix issues

--- src/routes/productsRouter.ts
+++ src/routes/productsRouter.ts
@@ -5,8 +5,8 @@ import { authToken } from '../middleware/response_middleware'
 const productsRouter = express.Router()
 const productcontroller = new ProductsController()
 
-productsRouter.get('/', authToken, productcontroller.getAll)
-productsRouter.get('/:id', authToken, productcontroller.getById)
+productsRouter.get('/', productcontroller.getAll)
+productsRouter.get('/:id', productcontroller.getById)
 productsRouter.post('/create', authToken, productcontroller.create)
 productsRouter.put('/:id', authToken, productcontroller.update)
 productsRouter.delete('/:id', authToken, productcontroller.delete)
